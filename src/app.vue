73f8954a469b5b00135c5ac0bc380589064e7647
syjs10 <350172898@qq.com>
2018-02-14 10:49:22 +0800

    test

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="test">{{text}}</div>
+</template>
+
+<script>
+    export default  {
+        return {
+            text: 'abc'
+        }
+    }
+</script>
+
+<style>
+    #test{
+        color: red;
+    }
+</style>
